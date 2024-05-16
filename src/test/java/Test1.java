46b98c60b6d9c3f60afdf8c6be76737883296f35
Your Name <you@example.com>
2024-05-16 21:58:38 +0530

    fifth commit

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,8 +1,14 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Sample execution");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
+		ChromeDriver driver = new ChromeDriver(options);
 
 	}
 
